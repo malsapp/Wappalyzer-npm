d2d66fe661418a7e2936adc9358bb4defe70a219
Mohamed Shaban <m.shaban@malsapp.com>
2017-04-03 17:45:28 +0200

    add all files

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const
+	path      = require('path'),
+	spawn     = require('child_process').spawn,
+	phantomjs = require('phantomjs-prebuilt');
+
+exports.run = function(args, callback, onClose) {
+	args.unshift.apply(args, [path.join(__dirname, 'driver.js'), '--web-security=false', '--load-images=false', '--ignore-ssl-errors=yes', '--ssl-protocol=any']);
+
+	var driver = phantomjs.exec.apply(this, args);
+
+	driver.stdout.on('data', (data) => {
+		callback(`${data}`, null);
+	});
+
+	driver.stderr.on('data', (data) => {
+		callback(null, `${data}`);
+	});
+  driver.on('close', () => {
+    onClose && onClose();
+  })
+}
+
+if ( !module.parent ) {
+	exports.run(process.argv.slice(2), function(stdout, stderr) {
+		if ( stdout ) {
+			process.stdout.write(stdout);
+		}
+
+		if ( stderr ) {
+			process.stderr.write(stderr);
+		}
+	});
+}
